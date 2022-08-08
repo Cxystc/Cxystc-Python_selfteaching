c69f44bca4ca28e333bfa273a9dac0d4bc18d4c2
Erytos <3174232122@qq.com>
2022-08-08 23:11:14 +0800

    update a new Python_project

---
+++ Python_project/Birthdaytools/Age_guessing_game.py
@@ -0,0 +1,11 @@
+import random
+num_random  = random.randint(0,100)
+variate = input('游戏开始，在0-99之间猜一个数字吧：')
+score  =  0 
+
+#比较大小
+if num_random == variate:
+    print('真棒！第一次就猜对了呢，让我们再来一次\n')
+    
+
+
