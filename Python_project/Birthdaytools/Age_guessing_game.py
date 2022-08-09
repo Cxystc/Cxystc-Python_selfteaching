4277b55974d189d8c0714fe31c288b56c18fe6de
Erytos <3174232122@qq.com>
2022-08-09 17:20:20 +0800

    fix some bug

--- Python_project/Birthdaytools/Age_guessing_game.py
+++ Python_project/Birthdaytools/Age_guessing_game.py
@@ -1,10 +1,8 @@
 import random
-#tag 方法循环：
-tag = True
 score = 0
 variate = random.randint(0,10)
 print('Game start ,now there is a new number range from 1 to 10,you are supposed to guess the number and try to find it due to the tips!\n')
-while tag:
+while True:
     print(variate)
     guess_number = int(input ('now input you number: '))
     #判断
