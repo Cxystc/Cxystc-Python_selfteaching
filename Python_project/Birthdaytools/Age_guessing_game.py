e0b626abf457655cf4e31371b107e578cd524a56
Erytos <3174232122@qq.com>
2022-08-09 17:16:01 +0800

    Finish the number guessing Age_guessing_game

--- Python_project/Birthdaytools/Age_guessing_game.py
+++ Python_project/Birthdaytools/Age_guessing_game.py
@@ -1,11 +1,37 @@
 import random
-num_random  = random.randint(0,100)
-variate = input('游戏开始，在0-99之间猜一个数字吧：')
-score  =  0 
+#tag 方法循环：
+tag = True
+score = 0
+variate = random.randint(0,10)
+print('Game start ,now there is a new number range from 1 to 10,you are supposed to guess the number and try to find it due to the tips!\n')
+while tag:
+    print(variate)
+    guess_number = int(input ('now input you number: '))
+    #判断
+    if guess_number > variate:
+        print('\nTry to guess a small one!\n')
+        continue
 
-#比较大小
-if num_random == variate:
-    print('真棒！第一次就猜对了呢，让我们再来一次\n')
-    
+    elif guess_number < variate:
+        print('\n5Try to guess a bigger one!\n')
+        continue
+    else:
+        print('Well done!\n')
+        variate = random.randint(0,10)
+        score += 1
+        print('Congratulations!  You have got %d points!\n'%score)
+        if score > 1 :
+            chioce = input('You have successfully guess for several time ,input "q" to quit or input "c" to continue:\n')
+            if chioce == 'q':
+                print('Thanks for playing!\n')
+                tag = False
+            elif chioce == 'c' :
+                continue
+            else : 
+                print ('?????We don\'t understand ,we will start a new game for you!\n')
+                continue
+
+
+
 
 
