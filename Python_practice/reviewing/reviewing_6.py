0e2bbe338b9f32e645daa7d8935c8a23458d5ed9
Erytos <3174232122@qq.com>
2022-09-05 22:35:21 +0800

    作用域练习

--- Python_practice/reviewing/reviewing_6.py
+++ Python_practice/reviewing/reviewing_6.py
@@ -9,3 +9,48 @@ def fun2():
     fun()
 fun2()
 #得到结果 1
+
+# #关键字声明
+# a = 1 
+# def fun_2():
+#     a += 1 
+#     print(a)
+# fun_2()
+# #local variable 'a' referenced before assignment
+
+a = 1 
+def fun_2():
+    global a 
+    a += 1 
+    print(a)
+fun_2()
+print(a)#a已经被修改
+
+#nonlocal
+def fun_3():
+    b = 1 
+    def fun_3_(b):#需要传入参数
+        b += 1 
+        print('b=',b)
+    fun_3_(b)
+    print('b=',b)
+fun_3()#此时b还是1
+
+def fun_4():
+    b = 1 
+    def fun_5():
+        nonlocal b
+        b += 1 
+        print('b =',b)
+    fun_5()
+    print('b =',b)#b已经变成了2
+fun_4()
+
+
+c = 3
+def fun_7():
+    nonlocal c
+    c += 1
+    print('c =',c)
+fun_7()
+        
